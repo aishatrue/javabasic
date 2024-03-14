ecc8506cd3b920f55d2e59af8917dde49a601492
aishatrue <140340401+aishatrue@users.noreply.github.com>
2024-03-14 10:42:48 +0900

    poly practice1

---
+++ src/poly/ex1/Cat.java
@@ -0,0 +1,9 @@
+package poly.ex1;
+
+public class Cat {
+    public void sound(){
+        System.out.println("동물 소리 테스트 시작");
+        System.out.println("야옹");
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
